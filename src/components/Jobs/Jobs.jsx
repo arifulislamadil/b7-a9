ed1b18a48191bd3fad2b3dc2b9097d45dcb80086
Ariful Islam Adil <adil.islam.arif@gmail.com>
2023-04-11 13:38:41 +0400

    add job list

--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Job from '../Job/Job';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Job from "../Job/Job";
 
 const Jobs = () => {
-    const jobs = useLoaderData();
-    return (
+  const jobs = useLoaderData();
+  return (
+    <div>
+      <div>
+      <div className="text-center">
+        <h3 className="text-3xl font-bold mt-6">Featured Jobs</h3>
+        <p className="mt-4">
+        Explore thousands of job opportunities with all the information you need. Its your future
+        </p>
+      </div>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3 mt-6">
-            {
-                jobs.map(job=><Job key={job.id} job={job}></Job>)
-            }
+          {jobs.map((job) => (
+            <Job key={job.id} job={job}></Job>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Jobs;
